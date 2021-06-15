bf2d4aa5d7c4a5b5e7134b734c9ad5a029afd335
TheChetnapls <bc.grewal@gmail.com>
2021-06-15 01:03:50 -0700

    process & filter update

--- cereal.js
+++ cereal.js
@@ -1,39 +1,25 @@
 const csv = require('csv-parser')
 const fs = require('fs')
-const prompt = require('prompt')
 const process = require('process')
 let cereals = []
 
-prompt.start()
+let calories = parseInt(process.argv[2])
 
-prompt.get('calories', function (err, calories) {
-    if (err) { return onErr(err); }
+fs.createReadStream('cereal.csv')
+    .pipe(csv({}))
+    .on('data', (data) => cereals.push(data))
+    .on('end', () => {
+        let limit = calories - 100
+        let yourCereals = cereals.filter(cal => (cal.calories <= calories || cal.calories >= limit))
 
-    fs.createReadStream('cereal.csv')
-        .pipe(csv({}))
-        .on('data', (data) => cereals.push(data))
-        .on('end', () => {
-            let yourCereals = []
-            for (let i = 0; i < cereals.length; i++) {
-                if (cereals[i].calories <= calories) {
-                    yourCereals.push(cereals[i])
-                }
+        let bestCereal = { rating: 0 }
+        for (let k = 0; k < yourCereals.length; k++) {
+            if (yourCereals[k].rating > bestCereal.rating) {
+                bestCereal = yourCereals[k]
             }
-            let bestCereal = { rating: 0 }
-            for (let k = 0; k < yourCereals.length; k++) {
-                if (yourCereals[k].rating > bestCereal.rating) {
-                    bestCereal = yourCereals[k]
-                }
-            }
-            console.log(bestCereal.name + '- Calories: ' + bestCereal.calories + ' Rating: ' + bestCereal.rating + ' out of 100')
-        })
-
-})
-
-function onErr(err) {
-    console.log(err);
-    return 1;
-}
+        }
+        console.log(bestCereal.name + '- Calories: ' + bestCereal.calories + ' Rating: ' + bestCereal.rating + ' out of 100')
+    })
 
 
 
