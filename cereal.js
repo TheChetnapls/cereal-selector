542c50fdcc1e106459a5c7e1949d8230694d3212
TheChetnapls <bc.grewal@gmail.com>
2021-06-15 19:28:58 -0700

    calorie limit update

--- cereal.js
+++ cereal.js
@@ -3,26 +3,35 @@ const fs = require('fs')
 const process = require('process')
 let cereals = []
 
-let calories = parseInt(process.argv[2])
+//take user calorie limit
+let cal = parseInt(process.argv[2])
 
 fs.createReadStream('cereal.csv')
     .pipe(csv({}))
-    .on('data', (data) => cereals.push(data))
-    .on('end', () => {
-        let limit = calories - 100
-        let yourCereals = cereals.filter(cal => (cal.calories <= calories || cal.calories >= limit))
+    .on('data', (data) => cereals.push(data)) //pushes data into existing array, as it reads the data
+    .on('end', () => { //happens on end, when there's no more data to be read
 
-        let bestCereal = { rating: 0 }
-        for (let k = 0; k < yourCereals.length; k++) {
-            if (yourCereals[k].rating > bestCereal.rating) {
-                bestCereal = yourCereals[k]
-            }
+        cereals.sort((a, b) => { return b.calories - a.calories }) //sorts cereals by calories in descending order 
+        //sets calories to highest value from data set if user input is higher than it
+        if (cal > cereals[0].calories) {
+            cal = cereals[0].calories
+            console.log('Calories too high, brought down to ' + cereals[0].calories + 'calories.')
         }
-        console.log(bestCereal.name + '- Calories: ' + bestCereal.calories + ' Rating: ' + bestCereal.rating + ' out of 100')
+
+        //sets lower bound limit on calorie intake
+        let limit = cal - 100
+        //creates new array with only cereals in calorie bounds
+        let yourCereals = cereals.filter(calCheck => {
+            return calCheck.calories <= cal && calCheck.calories >= limit
+        })
+        //checks for highest rating cereal in your cereal list
+        yourCereals.sort((a, b) => { return b.rating - a.rating })
+        console.log(yourCereals[0].name + ': Calories- ' + yourCereals[0].calories + ' Rating- ' + yourCereals[0].rating + ' out of 100;')
+        /*         let bestCereal = { rating: 0 }
+                for (let k = 0; k < yourCereals.length; k++) {
+                    if (yourCereals[k].rating > bestCereal.rating) {
+                        bestCereal = yourCereals[k]
+                    }
+                }
+                console.log(bestCereal.name + '- Calories: ' + bestCereal.calories + ' Rating: ' + bestCereal.rating + ' out of 100') */
     })
-
-
-
-
-
-
