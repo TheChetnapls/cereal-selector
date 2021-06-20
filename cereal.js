eaa098d34cd4726598b245d95fdff9b2490da630
TheChetnapls <36456949+TheChetnapls@users.noreply.github.com>
2021-06-20 11:18:56 -0700

    first commit

--- cereal.js
+++ cereal.js
@@ -1,37 +1,58 @@
-const csv = require('csv-parser')
 const fs = require('fs')
-const process = require('process')
-let cereals = []
 
-//take user calorie limit
+//split csv up by \r (carrige return, when the page moves back to left side, shows up at end of line)
+//and \n, newline (this shows up at the beginning of the newline)
+let cerealData = fs.readFileSync('cereal.csv', 'utf8').split('\r\n')
+//get the data keys from the first line
+const cerealKey = cerealData[0].split(',')
+//remove they key line from start of array
+cerealData.shift()
+
+//split the cereal data strings into arrays
+for (let i = 0; i < cerealData.length; i++) {
+    cerealData[i] = cerealData[i].split(',')
+}
+
+//change cereal array into objects with key value pairs
+for (let j = 0; j < cerealData.length; j++) {
+    //grab data from cereal and save it
+    let arrEle = cerealData[j]
+    //clear the data from array to make room for object key value pairs
+    cerealData[j] = {}
+    for (let k = 0; k < cerealKey.length; k++) {
+        //obj["keyname"] = value
+        //add key value pairs from data back to cereal array
+        cerealData[j][cerealKey[k]] = arrEle[k]
+    }
+}
+
+
 let cal = parseInt(process.argv[2])
 
-fs.createReadStream('cereal.csv')
-    .pipe(csv({}))
-    .on('data', (data) => cereals.push(data)) //pushes data into existing array, as it reads the data
-    .on('end', () => { //happens on end, when there's no more data to be read
+cerealData.forEach(element => {
+    if (isNaN(element.calories)) {
+        console.log(element)
+    }
+    //console.log(element.calories)
+})
 
-        cereals.sort((a, b) => { return b.calories - a.calories }) //sorts cereals by calories in descending order 
-        //sets calories to highest value from data set if user input is higher than it
-        if (cal > cereals[0].calories) {
-            cal = cereals[0].calories
-            console.log('Calories too high, brought down to ' + cereals[0].calories + 'calories.')
-        }
+/*
 
-        //sets lower bound limit on calorie intake
-        let limit = cal - 100
-        //creates new array with only cereals in calorie bounds
-        let yourCereals = cereals.filter(calCheck => {
-            return calCheck.calories <= cal && calCheck.calories >= limit
-        })
-        //checks for highest rating cereal in your cereal list
-        yourCereals.sort((a, b) => { return b.rating - a.rating })
-        console.log(yourCereals[0].name + ': Calories- ' + yourCereals[0].calories + ' Rating- ' + yourCereals[0].rating + ' out of 100;')
-        /*         let bestCereal = { rating: 0 }
-                for (let k = 0; k < yourCereals.length; k++) {
-                    if (yourCereals[k].rating > bestCereal.rating) {
-                        bestCereal = yourCereals[k]
-                    }
-                }
-                console.log(bestCereal.name + '- Calories: ' + bestCereal.calories + ' Rating: ' + bestCereal.rating + ' out of 100') */
-    })
+cerealData.sort((a, b) => { return b.calories - a.calories }) //sorts cereals by calories in descending order 
+//sets calories to highest value from data set if user input is higher than it
+if (cal > cerealData[0].calories) {
+    cal = cerealData[0].calories
+    console.log('Calories too high, brought down to ' + cerealData[0].calories + 'calories.')
+}
+
+
+//sets lower bound limit on calorie intake
+let limit = cal - 100
+//creates new array with only cereals in calorie bounds
+let yourCereals = cerealData.filter(calCheck => {
+    return calCheck.calories <= cal && calCheck.calories >= limit
+})
+//checks for highest rating cereal in your cereal list
+yourCereals.sort((a, b) => { return b.rating - a.rating })
+console.log(yourCereals[0].name + ': Calories- ' + yourCereals[0].calories + ' Rating- ' + yourCereals[0].rating + ' out of 100;')
+*/
